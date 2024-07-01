d621010829858ad673dde551dbd01b29b6dc9537
wiktormalyska <wiktormalyska03@gmail.com>
2024-07-01 21:04:13 +0200

    added register message

--- src/main/java/com/wiktormalyska/backend/controllers/login/LoginRegisterController.java
+++ src/main/java/com/wiktormalyska/backend/controllers/login/LoginRegisterController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginRegisterController {
@@ -21,10 +22,11 @@ public class LoginRegisterController {
     }
 
     @PostMapping("/register")
-    public String register(@ModelAttribute("user") User user, Model model) {
+    public String register(@ModelAttribute("user") User user, Model model, RedirectAttributes redirectAttributes) {
         String result = loginRegisterService.registerUser(user);
         model.addAttribute("message", result);
         if (result.equals("success")) {
+            redirectAttributes.addFlashAttribute("msg", "Użytkownik zarejestrowany pomyślnie!");
             return "redirect:/login";
         }
         return "register";
